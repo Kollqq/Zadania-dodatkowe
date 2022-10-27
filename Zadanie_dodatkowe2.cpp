2a30f92ddd3496205457afbfbea6babd3bad15b2
Anton <antonbybrest@gmail.com>
2022-10-27 22:24:22 +0200

    init

---
+++ Zadanie_dodatkowe2.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a;
+    double b;
+    double c;
+    double d;
+
+    cin >> a >> b >> c;
+
+    d = (a + b + c) / 3;
+
+    cout << d << endl;
+
+    return 0;
+}
+
---
+++ Zadanie_dodatkowe3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 3, b = 2, c = 4, d;
+	d = a++ + b - c % 3; //a= 3 ,b= 2 , c= 4 ,d= 4
+	d = ++a + b - c % 3; //a= 5 ,b= 2 , c= 4 ,d= 6
+	d = (b = c) + 2; //a= 5 ,b= 4 , c= 4 ,d= 6
+	return 0;
+}
